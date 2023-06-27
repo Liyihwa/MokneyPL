5265a4b9facc2bbd97ca4a2890fa822681db6a01
liyihwa <liyihwa04@163.com>
2023-06-28 07:42:51 +0800

    parser developing...

--- src/interpreter/parser/parser.go
+++ src/interpreter/parser/parser.go
@@ -8,15 +8,22 @@ import (
 	"fmt"
 )
 
+/*
+* Parser是语法分析器,他会一次输入一整行代码字符,并返回一个`Statement`
+* parseStatment()方法每次返回本行的Statment和error
+* ParseStatment()方法会调用parseStatment(),并维护curStatment和curError
+* Parser在创建(New())时会率先执行ParseStatment(),从而将curStatment和curError赋值
+ */
+
 type Parser struct {
 	l           *lexer.Lexer
-	curStatment ast.Statement
-	curError    error
+	curStatment ast.Statement // 当前语句
+	curError    error         // 当前语句的error
 }
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l}
-	p.parseStatment()
+	p.ParseStatment()
 	return p
 }
 
@@ -31,7 +38,6 @@ func (p *Parser) expectPopPeek(tokenType token.TokenType) (token.Token, bool) {
 	return p.l.Peek(), false
 }
 
-// Peek方法返回语法解析器顶
 func (p *Parser) Peek() ast.Statement {
 	return p.curStatment
 }
@@ -40,40 +46,40 @@ func (p *Parser) Empty() bool {
 	return p.curStatment == nil
 }
 
+func (p *Parser) pop() {
+	p.l.Pop()
+}
+
 // parseStatment方法一次解析一整行,将其转化为ast中的语句(Statment),目前只能解析let
-func (p *Parser) parseStatment() error {
-	for p.expectPeek(token.EOL) {
-		p.l.Pop()
+func (p *Parser) parseStatment() (ast.Statement, error) {
+	for _, ok := p.expectPopPeek(token.EOL); ok; _, ok = p.expectPopPeek(token.EOL) { //跳过空行
+
 	}
-	var errorG error
 	switch p.l.Peek().Type {
 	case token.LET:
-		p.curStatment, errorG = p.parseLet()
+		return p.parseLet()
 	case token.EOF:
-		p.curStatment = nil
-	case token.ILLEGAL: //出现非法token
-		p.curStatment = ast.NewIllegalStatment("Unknow token " + p.l.Peek().Literal)
-		errorG = errors.New(fmt.Sprintf("%s", p.curStatment))
+		return nil, nil
+	case token.ILLEGAL:
+		return ast.NewIllegalStatment("Unknow token " + p.l.Peek().Literal), fmt.Errorf("%s", p.curStatment)
 	}
-
-	return errorG
+	return nil, nil
 }
 
-// ParseStatment方法比起parseStatment多了对空行的处理
+// ParseStatment方法用来维护当前语句和当前语句的error
 func (p *Parser) ParseStatment() (ast.Statement, error) {
-	temp, err := p.curStatment, p.curError
-
-	p.curError = p.parseStatment()
-	return temp, err
+	statment, err := p.curStatment, p.curError
+	p.curStatment, p.curError = p.parseStatment()
+	return statment, err
 }
 
-/* ----------------
-*各种实现方法
+/*
+*这里写了不同的语句的实现方法,对于每一种语句(let语句,if语句等等),我们都为其实现一个函数
  */
 
 func (p *Parser) parseLet() (ast.Statement, error) {
 	res := ast.LetStatement{Token: p.l.Peek()}
-	p.l.Pop() // 跳过let
+	p.pop() // 跳过let
 
 	idToken := token.Token{}
 	if tok, flag := p.expectPopPeek(token.ID); flag { //解析id
